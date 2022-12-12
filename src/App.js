eadea1acd4b333b33e20039f1dcc13e1efc44d08
arslann <arslansahin.1907@gmail.com>
2022-12-12 20:06:27 +0300

    complete sidebar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import Sidebar from './components/sidebar/Sidebar';
+import { Sidebar } from './components';
 import './App.css';
+import { Highlights, Nextdays } from './containers';
 
 function App() {
-  return <Sidebar></Sidebar>;
+  return (
+    <>
+      <div className="app">
+        <Sidebar></Sidebar>
+        <div className="background">
+          <Nextdays></Nextdays>
+          <Highlights></Highlights>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import './sidebar.css';
 
+import icon from '../../assets/Shower.png';
+import { GoLocation } from 'react-icons/go';
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="wa__sidebar">
+      <div className="wa__sidebar-filter">
+        <div className="wa__sidebar-input">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search for places"
+          />
+        </div>
+        <div className="wa__sidebar-icon">
+          <img src={icon} alt="" />
+        </div>
+
+        <div className="wa__sidebar-container">
+          <div className="wa__sidebar-container-weather">
+            <p>15℃</p>
+            <p>Shower</p>
+          </div>
+          <div className="wa__sidebar-container-date">
+            <p>Today - Fri, 5 Jun</p>
+          </div>
+          <div className="wa__sidebar-container-location">
+            <GoLocation />
+            <p> Helsinki</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
