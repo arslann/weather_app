fc0c739d7aa381b8528be173dd1908f647f6064e
arslann <arslansahin.1907@gmail.com>
2022-12-14 14:57:40 +0300

    add nextdays container

--- src/containers/nextdays/Nextdays.jsx
+++ src/containers/nextdays/Nextdays.jsx
@@ -1,8 +1,66 @@
 import React from 'react';
 import './nextdays.css';
+import { Card } from '../../components';
+import icon from '../../assets/LightRain.png';
+import icon2 from '../../assets/Thunderstorm.png';
+import icon3 from '../../assets/Snow.png';
+import icon4 from '../../assets/Shower.png';
 
 function Nextdays() {
-  return <div>Nextdays</div>;
+  return (
+    <div className="wa__nextdays">
+      <Card>
+        <div className="wa__nextdays-title">Tomorrow</div>
+        <div className="wa__nextdays-img">
+          <img src={icon} alt="" />
+        </div>
+        <div className="wa__nextdays-temp">
+          <span>16°C</span>
+          <span>11°C</span>
+        </div>
+      </Card>
+      <Card>
+        <div className="wa__nextdays-title">Sun, 7 Jun</div>
+        <div className="wa__nextdays-img">
+          <img src={icon2} alt="" />
+        </div>
+        <div className="wa__nextdays-temp">
+          <span>16°C</span>
+          <span>11°C</span>
+        </div>
+      </Card>
+      <Card>
+        <div className="wa__nextdays-title">Mon, 8 Jun</div>
+        <div className="wa__nextdays-img">
+          <img src={icon4} alt="" />
+        </div>
+        <div className="wa__nextdays-temp">
+          <span>16°C</span>
+          <span>11°C</span>
+        </div>
+      </Card>
+      <Card>
+        <div className="wa__nextdays-title">Tue, 9 Jun</div>
+        <div className="wa__nextdays-img">
+          <img src={icon4} alt="" />
+        </div>
+        <div className="wa__nextdays-temp">
+          <span>16°C</span>
+          <span>11°C</span>
+        </div>
+      </Card>
+      <Card>
+        <div className="wa__nextdays-title">Wed, 10 Jun</div>
+        <div className="wa__nextdays-img">
+          <img src={icon4} alt="" />
+        </div>
+        <div className="wa__nextdays-temp">
+          <span>16°C</span>
+          <span>11°C</span>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 export default Nextdays;
