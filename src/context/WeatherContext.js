dbfb916e9650a060b3ceafb09122e75ce097a1c9
arslann <arslansahin.1907@gmail.com>
2022-12-21 16:21:13 +0300

    update https protocol

--- src/context/WeatherContext.js
+++ src/context/WeatherContext.js
@@ -6,10 +6,9 @@ export const WeatherProvider = ({ children }) => {
   const [city, setCity] = useState('istanbul');
   const [weather, setWeather] = useState({});
 
-  //Fetch weather
   const fetchWeather = useCallback(async () => {
     const response =
-      await fetch(`http://api.weatherapi.com/v1/forecast.json?key=13d8fa5f20674ebd9d9111535221912&q=${city}&days=5&aqi=no&alerts=no
+      await fetch(`https://api.weatherapi.com/v1/forecast.json?key=13d8fa5f20674ebd9d9111535221912&q=${city}&days=5&aqi=no&alerts=no
     `);
 
     const data = await response.json();
@@ -21,6 +20,17 @@ export const WeatherProvider = ({ children }) => {
   useEffect(() => {
     fetchWeather();
   }, [city, fetchWeather]);
+
+  // const fetchWeather = async () => {
+  //   const response =
+  //     await fetch(`http://api.weatherapi.com/v1/forecast.json?key=13d8fa5f20674ebd9d9111535221912&q=${city}&days=5&aqi=no&alerts=no
+  //   `);
+
+  //   const data = await response.json();
+
+  //   setWeather(data);
+  //   console.log(data);
+  // };
 
   return (
     <WeatherContext.Provider
